1f28c87186fc878c35788acaa1b173857318c10b
drakeet <drakeet.me@gmail.com>
2015-03-27 12:36:42 +0800

    Initial commit

---
+++ library/src/main/java/me/drakeet/library/UIButton.java
@@ -0,0 +1,110 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 drakeet (http://drakeet.me)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package me.drakeet.library;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+/**
+ * Created by drakeet on 3/27/15.
+ */
+public class UIButton extends Button {
+
+    private int WIDTH;
+    private int HEIGHT;
+    private int PAINT_ALPHA = 90;
+
+    private int mPressedColor;
+    private Paint mPaint;
+    private int mAction = -1;
+
+    public UIButton(Context context) {
+        super(context);
+    }
+
+    public UIButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public UIButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(final Context context, final AttributeSet attrs) {
+        if (isInEditMode())
+            return;
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.UIButton);
+        mPressedColor = typedArray.getColor(R.styleable.UIButton_color_pressed, getResources().getColor(R.color.color_pressed));
+        PAINT_ALPHA = typedArray.getInteger(R.styleable.UIButton_alpha_pressed, PAINT_ALPHA);
+        typedArray.recycle();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(mPressedColor);
+        this.setWillNotDraw(false);
+
+        this.setDrawingCacheEnabled(true);
+        this.setClickable(true);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        WIDTH = w;
+        HEIGHT = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mPaint == null) return;
+        if (mAction == MotionEvent.ACTION_DOWN) {
+            mPaint.setAlpha(PAINT_ALPHA);
+        } else {
+            mPaint.setAlpha(0);
+        }
+        canvas.drawRect(0, 0, WIDTH, HEIGHT, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mAction = event.getAction();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                invalidate();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+}
